7ef250b1fd9bf78e6b3c3bf49d8815b6b5443c6f
Jeremy Le François <jelefra@users.noreply.github.com>
2021-04-06 20:56:27 +0100

    Improve build performance
    "Apply loaders to the minimal number of modules necessary."
    
    See https://webpack.js.org/guides/build-performance/

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,12 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
+        include: path.resolve(__dirname, 'src'),
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
