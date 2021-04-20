e190833d16089149da89fe647d97bf624f95a885
Jeremy Le François <jelefra@users.noreply.github.com>
2021-04-20 22:16:05 +0100

    Update `dev` script command
    Bring the dev environment closer to production by removing
    html-webpack-plugin.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
@@ -10,13 +9,7 @@ module.exports = merge(common, {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-  ],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
