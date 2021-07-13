ed631cdeef661bc069ec1a701e9d3b021833215b
Jeremy Le François <jelefra@users.noreply.github.com>
2021-07-13 22:12:48 +0100

    Simplify webpack copying rules
    See https://github.com/webpack-contrib/copy-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -40,55 +40,19 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new CopyPlugin({
       patterns: [
-        { from: 'static/robots.txt', to: 'robots.txt' },
-        { from: 'static/sitemap.xml', to: 'sitemap.xml' },
-        { from: 'static/manifest.webmanifest', to: 'manifest.webmanifest' },
-        { from: 'static/favicon.ico', to: 'favicon.ico' },
-        { from: 'static/icon.svg', to: 'icon.svg' },
-        { from: 'static/icon-192.png', to: 'icon-192.png' },
-        { from: 'static/icon-512.png', to: 'icon-512.png' },
-        { from: 'static/apple-touch-icon.png', to: 'apple-touch-icon.png' },
         {
-          from:
-            'src/posts/21-06-18-homeServerEvolutionStoneAge/raspberry-pi-mobile.jpg',
-          to: 'home-server-evolution-stone-age/raspberry-pi-mobile.jpg',
+          from: path.resolve(__dirname, 'static'),
+          globOptions: { ignore: ['**/original/**'] },
         },
         {
-          from:
-            'src/posts/21-06-18-homeServerEvolutionStoneAge/raspberry-pi-2x.jpg',
-          to: 'home-server-evolution-stone-age/raspberry-pi-2x.jpg',
-        },
-        {
-          from:
-            'src/posts/21-06-18-homeServerEvolutionStoneAge/raspberry-pi-1x.jpg',
-          to: 'home-server-evolution-stone-age/raspberry-pi-1x.jpg',
-        },
-        {
-          from:
-            'src/posts/21-06-18-homeServerEvolutionStoneAge/raspberry-pi-mobile.avif',
-          to: 'home-server-evolution-stone-age/raspberry-pi-mobile.avif',
-        },
-        {
-          from:
-            'src/posts/21-06-18-homeServerEvolutionStoneAge/raspberry-pi-2x.avif',
-          to: 'home-server-evolution-stone-age/raspberry-pi-2x.avif',
-        },
-        {
-          from:
-            'src/posts/21-06-18-homeServerEvolutionStoneAge/raspberry-pi-1x.avif',
-          to: 'home-server-evolution-stone-age/raspberry-pi-1x.avif',
-        },
-        {
-          from: 'static/img/raspberry-pi-1x1.jpg',
-          to: 'img/raspberry-pi-1x1.jpg',
-        },
-        {
-          from: 'static/img/raspberry-pi-4x3.jpg',
-          to: 'img/raspberry-pi-4x3.jpg',
-        },
-        {
-          from: 'static/img/raspberry-pi-16x9.jpg',
-          to: 'img/raspberry-pi-16x9.jpg',
+          from: '**/*.{jpg,avif}',
+          to: 'home-server-evolution-stone-age',
+          context: path.resolve(
+            __dirname,
+            'src',
+            'posts',
+            '21-06-18-homeServerEvolutionStoneAge'
+          ),
         },
       ],
     }),
